725368c0fee15ab497ea219a21c6a7c27510da35
Jacky <jackyman1997@gmail.com>
2021-06-13 15:37:00 +0100

    Delete do.py

--- 3. classfier/do.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-def get_data(): 
-    folder_path = "./data/"
-    basestat_filename = folder_path + "baseStats.json"
-    match_filename = folder_path + "match_ok.json"
-
-    with open(folder_path+basestat_filename, 'r') as f: 
